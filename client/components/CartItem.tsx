f48c29f9eebc9d6f3d424aa7060184a0c181ca1c
le-dat <lqdat87@gmail.com>
2023-04-26 21:35:23 +0700

    update css 0.1

--- client/components/CartItem.tsx
+++ client/components/CartItem.tsx
@@ -32,8 +32,8 @@ const CartItem: React.FC<CartProductIProps> = ({ id, product, quantity, size, cr
   };
 
   return (
-    <div className="flex gap-3 p-3 border-b hover:bg-gray-100 cursor-pointer transition rounded-lg">
-      <Link href={`/product/${product.slug}`} className="[-webkit-tap-highlight-color:transparent]">
+    <div className="flex gap-3 p-3 border-b hover:bg-gray-100 cursor-pointer transition rounded-lg [-webkit-tap-highlight-color:transparent]">
+      <Link href={`/product/${product.slug}`} className="">
         <Image
           src={product.thumbnail.data.attributes.url}
           alt={product.name}
