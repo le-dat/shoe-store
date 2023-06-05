25bfaa0d0d21f811d02a986a8d7e9f67f5ba6147
le-dat <lqdat87@gmail.com>
2023-06-05 22:14:08 +0700

    update css 0.7

--- client/components/shared/ProductDetailsCarousel.tsx
+++ client/components/shared/ProductDetailsCarousel.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 import { ImageDataIProps } from "@/types"
 
